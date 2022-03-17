569a70efbfa361f8f5fd70fdb93038c0b30aa36d
Apurbo Mitra <apurbo.mitra@mtxb2b.com>
2022-03-17 23:19:06 +0530

    update name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/satisfic-frontend-developer-task"));
+app.use(express.static("./dist/Satisfic-Frontend-Developer-Task"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/satisfic-frontend-developer-task/" })
+  res.sendFile("index.html", { root: "dist/Satisfic-Frontend-Developer-Task/" })
 );
 
 // Start the app by listening on the default Heroku port
